bc44e2a8c93b225869d803dc7f3c3936aac98c80
Maxim Lyuchin <maxim.lyuchin@gmail.com>
2019-03-08 21:44:45 +0300

    Removed CF7 support - not actual any more

--- morse.php
+++ morse.php
@@ -15,7 +15,6 @@ defined( 'ABSPATH' ) or exit;
 
 include_once plugin_dir_path( __FILE__ ) . 'settings.php';
 include_once plugin_dir_path( __FILE__ ) . 'telegram-functions.php';
-include_once plugin_dir_path( __FILE__ ) . 'cf7-functions.php';
 include_once plugin_dir_path( __FILE__ ) . 'wp-mail.php';
 
 function morse_init()
@@ -23,16 +22,6 @@ function morse_init()
 	$options = get_option('morse');
 
 	load_plugin_textdomain( 'morse', false, basename( dirname(__FILE__) ).'/languages' );
-
-	if ( defined('WPCF7_VERSION') && $options['cf7_enable'] ) {
-		// Hook to CF7 send mail
-		add_action( 'wpcf7_before_send_mail', 'morse_cf7_send', 10, 1 );
-
-		// Skip CF7 mail
-		if ( $options['cf7_skip_mail']) {
-			add_filter( 'wpcf7_skip_mail', '__return_true', 10 );
-		}
-	}
 
 	do_action( 'morse_init' );
 }
